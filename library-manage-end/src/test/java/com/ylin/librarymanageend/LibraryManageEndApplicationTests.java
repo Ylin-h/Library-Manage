8bfa1ec71373f5e2dcdf2dc8e8ca9a2d062a817b
Ylin <1852847329@qq.com>
2024-10-17 19:48:27 +0800

    图书管理后端

---
+++ library-manage-end/src/test/java/com/ylin/librarymanageend/LibraryManageEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ylin.librarymanageend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManageEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
